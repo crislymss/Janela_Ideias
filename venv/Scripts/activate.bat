6b247dfbd05bd1b921b7be77b8ec7e52d934e690
Kawan S. Dias <kawan.dias@ufpi.edu.br>
2024-07-17 21:38:35 -0300

    Mudança de Database
    aqui foi alterado do SQL Lite pro Postgree, detalhe apagou toda a base de dados, logo, tem que cadastrar tudo de novo, hehehe.

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Erik\Desktop\projetoweb\Projeto_Novo\venv
+set VIRTUAL_ENV=C:\Users\Kawan\OneDrive\Área de Trabalho\Julio\Projeto_Novo\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
