ec299bb4bb69cc58e032f7e95fbd0e46ba1f5a58
lannyily <erlanny.rego@ufpi.edu.br>
2024-10-11 22:39:14 -0300

    socorro deus

--- app/urls.py
+++ app/urls.py
@@ -29,16 +29,16 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('inbate/admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.index, name='home'),
-    path('inbate/teste/', views.teste, name='teste'),
+    path('teste/', views.teste, name='teste'),
     path('catalogo_startup/', views.catalogo_startup, name='catalogo_startup'),
-    path('inbate/perfil_startup/<str:nome>/',
+    path('perfil_startup/<str:nome>/',
          views.perfil_startup, name='perfil_startup'),
-    path('inbate/criar_startup/', views.criar_startup, name='criar_startup'),
-    path('inbate/perfil_projeto/<str:startup_nome>/<str:projeto_nome>/',
+    path('criar_startup/', views.criar_startup, name='criar_startup'),
+    path('perfil_projeto/<str:startup_nome>/<str:projeto_nome>/',
          views.perfil_projeto, name='perfil_projeto'),
-    path('inbate/login/', login_view, name='login'),
+    path('login/', login_view, name='login'),
     path('perfil_startup/<str:nome>/', perfil_startup, name='perfil_startup'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
